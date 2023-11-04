2f3ea5212798c45a07780e4bdbc4a0c9114f8c0b
Caro-L-dev <liardcar@gmail.com>
2023-11-04 15:52:37 +0100

    chore: generate password

--- Passwordgenerator/index.js
+++ Passwordgenerator/index.js
@@ -4,7 +4,8 @@ const askPasswordLength = () => {
   const passwordLength = Number(prompt("Combien de caracteres ? "));
   if (
     Number.isNaN(passwordLength) ||
-    (passwordLength >= 8 && passwordLength <= 36)
+    passwordLength <= 8 ||
+    passwordLength >= 36
   ) {
     throw new Error("Veuillez choisir entre 8 et 36 caractères maximum.");
   }
@@ -29,6 +30,63 @@ const yesOrNoValue = (value) => {
 const askSpecialChars = () => {
   const value = prompt("Caracteres speciaux ? (y/n) ");
   return yesOrNoValue(value);
+};
+
+const getRandomValue = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
+const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const SPECIALS = "!@#$%^&*()";
+const NUMBERS = "0123456789";
+
+const generatePassword = (
+  passwordLength,
+  containNumber,
+  containUpperCase,
+  containSpecialChars
+) => {
+  let charset = LOWERCASE;
+
+  if (containUpperCase) {
+    charset += UPPERCASE;
+  }
+
+  if (containSpecialChars) {
+    charset += SPECIALS;
+  }
+
+  if (containNumber) {
+    charset += NUMBERS;
+  }
+
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const randomCharacter = charset[getRandomValue(charset.length - 1)];
+    password += randomCharacter;
+  }
+
+  if (containUpperCase && password.toLowerCase() === password) {
+    return generatePassword(
+      passwordLength,
+      containNumber,
+      containSpecialChars,
+      containUpperCase
+    );
+  }
+
+  if (containNumber && !password.split("").some((p) => NUMBERS.includes(p))) {
+    {
+      return generatePassword(
+        passwordLength,
+        containNumber,
+        containSpecialChars,
+        containUpperCase
+      );
+    }
+  }
+  return password;
 };
 
 const askNumbers = () => {
@@ -64,6 +122,13 @@ const main = () => {
       console.log(error.message);
     }
   }
+  const password = generatePassword(
+    passwordLength,
+    containNumber,
+    containUpperCase,
+    containSpecialChars
+  );
+  console.log(`Votre mot de passe généré est :${password}`);
 };
 
 main();
